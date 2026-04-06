ba95e33118a7fe9f522a2723b69f05902ed6f43e
SANCHi0015 <136969228+SANCHi0015@users.noreply.github.com>
2026-04-06 18:06:01 +0530

    Refactor ML app with additional encoders and prediction logic

--- ml_app.py
+++ ml_app.py
@@ -3,32 +3,51 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+# Load dataset
 df = pd.read_csv("Final_Project.csv")
-pickle_in = open('regression_model.pkl','rb')
-reg = pickle.load(pickle_in)
 
-def predict_price(Area_SqFt,Floor_No,Bedroom):
-    x = np.zeros(7)
-    
-    x[0] = Area_SqFt
-    x[1] = Floor_No
-    x[2] = Bedroom
+# Load model
+reg = pickle.load(open('regression_model.pkl','rb'))
 
-    return reg.predict([x])[0]
+# Load encoders
+le_region = pickle.load(open('le_region.pkl','rb'))
+le_property_age = pickle.load(open('le_property_age.pkl','rb'))
+le_area_type = pickle.load(open('le_area_type.pkl','rb'))
 
+# ----------- PREDICTION FUNCTION -----------
+def predict_price(region, area_sqft, floor_no, bedroom, property_age, bathroom):
+
+    region = le_region.transform([region])[0]
+    property_age = le_property_age.transform([property_age])[0]
+
+    area_type = df['Area_Tpye'].mode()[0]
+    area_type = le_area_type.transform([area_type])[0]
+
+    rate_sqft = df['Rate_SqFt'].mean()
+
+    x = [[region, property_age, area_type,
+          area_sqft, rate_sqft, floor_no, bedroom, bathroom]]
+
+    return reg.predict(x)[0]
+
+
+# ----------- STREAMLIT UI -----------
 def run_ml_app():
     st.subheader('Please enter the required details :')
-    Location = st.selectbox('Select the Location',(df['Region'].sort_values().unique()))
-    Area_SqFt = st.slider("Select Total Area in SqFt",500,int(max(df['Area_SqFt'])),step=100)    
-    Floor_No = st.selectbox("Enter Floor Number",(df['Floor_No'].sort_values().unique()))
-    Bathroom = st.selectbox("Enter Number of Bathroom",(df['Bathroom'].sort_values().unique()))
-    Bedroom = st.selectbox("Enter Number of Bedroom",(df['Bedroom'].sort_values().unique()))
-    Property_Age = st.selectbox('Select the Property Age',(df['Property_Age'].sort_values().unique()))
-    result= ""
-  
+
+    Location = st.selectbox('Select the Location', df['Region'].sort_values().unique())
+    Area_SqFt = st.slider("Select Total Area in SqFt", 500, int(max(df['Area_SqFt'])), step=100)
+    Floor_No = st.selectbox("Enter Floor Number", df['Floor_No'].sort_values().unique())
+    Bathroom = st.selectbox("Enter Number of Bathroom", df['Bathroom'].sort_values().unique())  # not used
+    Bedroom = st.selectbox("Enter Number of Bedroom", df['Bedroom'].sort_values().unique())
+    Property_Age = st.selectbox('Select the Property Age', df['Property_Age'].sort_values().unique())
+
+    result = ""
+
     if st.button("Calculate Price"):
-        result = predict_price(Area_SqFt,Floor_No,Bedroom)
-    st.success('Total Price in Lakhs : {}'.format(result))
-    
-if __name__=='__main__':
+        result = predict_price(Location, Area_SqFt, Floor_No, Bedroom, Property_Age, Bathroom)
+        st.success('Total Price in Crores : {}'.format(round((result/10) - 5, 2)))
+
+
+if __name__ == '__main__':
     run_ml_app()
